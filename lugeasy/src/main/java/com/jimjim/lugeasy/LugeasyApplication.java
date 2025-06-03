6b3828ba16f6f346d707e278399a942cd21ae318
KyungsooLee <jmtkd9196@gmail.com>
2025-06-03 18:46:29 +0900

    [feat] init spring project

---
+++ lugeasy/src/main/java/com/jimjim/lugeasy/LugeasyApplication.java
@@ -0,0 +1,13 @@
+package com.jimjim.lugeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LugeasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LugeasyApplication.class, args);
+	}
+
+}
