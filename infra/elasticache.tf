d3b0020bfcc598960040227e020e293eae65d846
unknown <gkqkehs10@naver.com>
2025-10-26 22:12:44 +0900

    delete redis from tf

--- infra/elasticache.tf
+++ infra/elasticache.tf
@@ -1,8 +1,8 @@
-resource "aws_elasticache_cluster" "lugeasy_redis" {
-  cluster_id        = "lugeasy-redis"
-  engine            = "redis"
-  node_type         = "cache.t3.micro"
-  num_cache_nodes   = 1
-  port              = 6379
-  apply_immediately = true
-}
+# resource "aws_elasticache_cluster" "lugeasy_redis" {
+#   cluster_id        = "lugeasy-redis"
+#   engine            = "redis"
+#   node_type         = "cache.t3.micro"
+#   num_cache_nodes   = 1
+#   port              = 6379
+#   apply_immediately = true
+# }
