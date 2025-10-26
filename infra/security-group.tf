10536012b6207fee473b83742f70c2a7803bbe93
unknown <gkqkehs10@naver.com>
2025-10-26 22:15:36 +0900

    [infra] add provider to tf

--- infra/security-group.tf
+++ infra/security-group.tf
@@ -1,40 +1,40 @@
-# resource "aws_security_group" "lugeasy_security_group" {
-#   name        = "lugeasy-security-group"
-#   description = "lugeasy security group"
-#   vpc_id      = "vpc-0d9faf2a4f2631b63" 
-# }
+resource "aws_security_group" "lugeasy_security_group" {
+  name        = "lugeasy-security-group"
+  description = "lugeasy security group"
+  vpc_id      = "vpc-0d9faf2a4f2631b63" 
+}
 
-# # ingress rule
-# resource "aws_vpc_security_group_ingress_rule" "lugeasy_22_ingress_rule" {
-#   security_group_id = aws_security_group.lugeasy_security_group.id
-#   cidr_ipv4         = "0.0.0.0/0"
-#   from_port         = 22
-#   ip_protocol       = "tcp"
-#   to_port           = 22
-# }
+# ingress rule
+resource "aws_vpc_security_group_ingress_rule" "lugeasy_22_ingress_rule" {
+  security_group_id = aws_security_group.lugeasy_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
 
-# resource "aws_vpc_security_group_ingress_rule" "lugeasy_80_ingress_rule" {
-#   security_group_id = aws_security_group.lugeasy_security_group.id
-#   cidr_ipv4         =  "0.0.0.0/0"
-#   from_port         = 80
-#   ip_protocol       = "tcp"
-#   to_port           = 80
-# }
+resource "aws_vpc_security_group_ingress_rule" "lugeasy_80_ingress_rule" {
+  security_group_id = aws_security_group.lugeasy_security_group.id
+  cidr_ipv4         =  "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
 
-# resource "aws_vpc_security_group_ingress_rule" "lugeasy_443_ingress_rule" {
-#   security_group_id = aws_security_group.lugeasy_security_group.id
-#   cidr_ipv4         = "0.0.0.0/0"
-#   from_port         = 443
-#   ip_protocol       = "tcp"
-#   to_port           = 443
-# }
+resource "aws_vpc_security_group_ingress_rule" "lugeasy_443_ingress_rule" {
+  security_group_id = aws_security_group.lugeasy_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
 
-# # egress rule
-# resource "aws_security_group_rule" "lugeasy_any_open_egress_rule" {
-#   type              = "egress"
-#   security_group_id = aws_security_group.lugeasy_security_group.id
-#   from_port         = 0
-#   to_port           = 0
-#   protocol          = "-1"
-#   cidr_blocks       = ["0.0.0.0/0"]
-# }
+# egress rule
+resource "aws_security_group_rule" "lugeasy_any_open_egress_rule" {
+  type              = "egress"
+  security_group_id = aws_security_group.lugeasy_security_group.id
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
