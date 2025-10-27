a0c783ce937ef40380b61c4a860fe14d134c8c4a
unknown <gkqkehs10@naver.com>
2025-10-27 23:29:21 +0900

    fix typo

--- infra/data.tf
+++ infra/data.tf
@@ -3,18 +3,6 @@ data "aws_vpc" "lugeasy_vpc" {
     cidr_block = "10.0.0.0/24"
 }
 
-data "aws_internet_gateway" "lugeasy_internet_gateway" {
-    id = "igw-0024cce84865cc19c"
-}
-
-data "aws_route_table" "lugeasy_public_route_table" {
-    id = "rtb-0fb5627e2f2982d56"
-}
-
-data "aws_route_table" "lugeasy_private_route_table" {
-    id = "rtb-0edf0de8396b515f7"
-}
-
 data "aws_subnet" "lugeasy_public_subnet" {
     id = "subnet-03c09bf8b674e323e"
 }
