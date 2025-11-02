6fb8c83e4cd8f3899a119a3e07e1d8a6ad5ed22f
unknown <gkqkehs10@naver.com>
2025-11-02 14:23:07 +0900

    fix

--- infra/data.tf
+++ infra/data.tf
@@ -1,12 +1,12 @@
 data "aws_vpc" "lugeasy_vpc" {
-    id = "vpc-010ae6b4cbc4493ca"
+    id = "vpc-0e2383015022f184e"
     cidr_block = "10.0.0.0/24"
 }
 
 data "aws_subnet" "lugeasy_public_subnet" {
-    id = "subnet-03c09bf8b674e323e"
+    id = "subnet-0f273e6148db9b268"
 }
 
 data "aws_subnet" "lugeasy_private_subnet" {
-    id = "subnet-0b828c2949ebf9f57"
+    id = "subnet-0f9496ed64852d32f"
 }
